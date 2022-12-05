8e2c38f8572fbd4ebbd9efcf7a17a526da0e71ed
sahra tehran <sahra.tehrani2020@yahoo.com>
2022-12-05 16:38:39 +0330

    modal & total done!

--- src/js/ProductView.js
+++ src/js/ProductView.js
@@ -7,6 +7,9 @@ const addNewProductBtn = document.querySelector('.add-new-product-btn');
 const productCategory = document.getElementById('category-product');
 const searchInput = document.querySelector('#search-product');
 const selectedSort = document.querySelector('#sort-products');
+const modalProduct = document.querySelector('.modal-products');
+const backDrop = document.querySelector('.backdrop');
+const totalProductsNumber = document.querySelector('.total-products');
 
 
 class ProductView{
@@ -50,14 +53,32 @@ class ProductView{
                 <button class="delete-product btn" data-id='${item.id}'>Delete</button>
             </div>
         </div>`;
+
+        
     })
+
+    let totalProducts = 0;
+    products.forEach(item =>{
+        totalProducts += parseInt(item.quantity);
+    })
+    // console.log(totalProducts);
+    totalProductsNumber.innerText = totalProducts;
+    if (totalProducts<1){
+        totalProductsNumber.style.visibility="hidden";
+    }else{
+        totalProductsNumber.style.visibility="visible";
+    }
+
+
     productsDom.innerHTML = result;
     const deleteProdctBtns =[... document.querySelectorAll('.delete-product')]
-    // console.log(deleteProdctBtns)
     deleteProdctBtns.forEach( item => {
         item.addEventListener('click' ,(e) => this.deleteProduct(e))
     })
-    // deleteProdctBtns.forEach( item => item.addEventListener('click' , e => console.log(e.target)))
+    const editProductBtns =[... document.querySelectorAll('.edit-product')]
+    editProductBtns.forEach(item =>{
+        item.addEventListener('click' , e => this.editProduct(e))
+    })
     }
     searchProducts(e){
         const value = e.target.value.trim().toLowerCase();
@@ -79,6 +100,48 @@ class ProductView{
         this.setApp();
         this.createProductList(this.products);
     }
+    editProduct(e){
+        e.preventDefault();
+        const productId =e.target.dataset.id;
+        backDrop.style.display = "block" ;
+        modalProduct.style.visibility= "visible";
+        console.log(productTitle.value);
+
+        const categories= Storage.getAllCategories();       
+        let option =`<option class="category-product-option" value="">Select a category</option>` 
+        categories.forEach(element => {
+           option +=`<option class="category-product-option" value="${element.id}">${element.title}</option>`;
+        });
+
+        const selectedProduct = Storage.getAllProducts().find( c => parseInt(c.id) === parseInt(productId));
+        const div = document.createElement('div');
+        div.className ="modal-product";
+        let result=`<label class="label" for="product-title-edit">Edit Product Title</label>
+        <input  type="text" placeholder="product title" class="product-title-edit" value=${selectedProduct.title} id="product-title-edit" />
+        <label class="label" for="product-quantity-edit" >Edit Quantity</label>
+        <input type="number" placeholder="0" max="100" min="1" value=${selectedProduct.quantity} id="product-quantity-edit" />
+        <label class="label" for="category-product-edit">Edit Category</label>
+        <select id="category-product-edit" value=>${option}</select>
+        <div class="btns">
+            <button class="cancel-edit-product-btn btn">Cancel</button>
+            <button class="add-new-product-btn btn">Edit Product</button>
+        </div>`;
+        div.innerHTML=result;
+        modalProduct.appendChild(div)
+
+
+
+        const cancelModalBtn = [...document.querySelectorAll('.cancel-edit-product-btn')];
+        cancelModalBtn.forEach( item =>{
+            item.addEventListener('click' , e => {
+        e.preventDefault();
+        backDrop.style.display = "none" ;
+        modalProduct.style.visibility= "hidden";
+        modalProduct.removeChild(div);
+            })
+        })
+    }
+
 }
 
 export default new ProductView();
